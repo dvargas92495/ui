124e12c8bf9e7a53e35a9d2accdfb64388ff3233
David Vargas <dvargas92495@gmail.com>
2020-10-15 18:59:41 -0400

    Export to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as AppBar } from "./components/AppBar";
+export { default as Footer } from "./components/Footer";
 export { default as Logo } from "./components/Logo";
+export { default as Main } from "./components/Main";
 export { default as Root } from "./components/Root";
 export { default as ThemeProvider } from "./components/ThemeProvider";
