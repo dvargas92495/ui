6c395ead9d40a377f91612f40d74c6a3088c397d
David Vargas <dvargas92495@gmail.com>
2020-10-15 11:51:08 -0400

    Export Logo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as AppBar } from "./components/AppBar";
+export { default as Logo } from "./components/Logo";
 export { default as ThemeProvider } from "./components/ThemeProvider";
