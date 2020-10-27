0acd88d4f1aa8430964ba488f1c053ce2e71d488
David Vargas <dvargas92495@gmail.com>
2020-10-27 19:49:04 -0400

    Drawer children

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -9,10 +9,11 @@ import ListItemText from "@material-ui/core/ListItemText";
 
 const width = 240;
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   drawer: {
     width,
     flexShrink: 0,
+    zIndex: theme.zIndex.appBar - 1,
   },
   drawerPaper: {
     width,
@@ -20,30 +21,45 @@ const useStyles = makeStyles(() => ({
   drawerContainer: {
     overflow: "auto",
   },
+  content: {
+    paddingLeft: width,
+  },
 }));
 
-const NavigationDrawer = ({ items }: { items: string[] }) => {
+const NavigationDrawer = ({
+  items,
+  children,
+}: {
+  items: string[];
+  children: React.ReactNode;
+}) => {
   const classes = useStyles();
   return (
-    <Drawer
-      className={classes.drawer}
-      variant={isMobile ? "persistent" : "permanent"}
-      classes={{
-        paper: classes.drawerPaper,
-      }}
-      anchor="left"
-    >
-      <Toolbar />
-      <div className={classes.drawerContainer}>
-        <List>
-          {items.map((text, index) => (
-            <ListItem button key={index}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    </Drawer>
+    <>
+      <Drawer
+        className={classes.drawer}
+        variant={isMobile ? "persistent" : "permanent"}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+        PaperProps={{
+          variant: "outlined",
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {items.map((text, index) => (
+              <ListItem button key={index}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      <div className={classes.content}>{children}</div>
+    </>
   );
 };
 
