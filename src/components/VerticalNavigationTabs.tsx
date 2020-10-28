9f7b2dcfbfa2bca668e65c3027d0209d6a887bbc
David Vargas <dvargas92495@gmail.com>
2020-10-27 21:47:15 -0400

    VerticalNavigationTabs spacing

--- src/components/VerticalNavigationTabs.tsx
+++ src/components/VerticalNavigationTabs.tsx
@@ -1,6 +1,20 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    marginTop: theme.spacing(-8),
+  },
+  tabs: {
+    minWidth: theme.spacing(40),
+  },
+  tabPanel: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const VerticalNavigationTabs = ({
   items,
@@ -10,19 +24,20 @@ const VerticalNavigationTabs = ({
   children: React.ReactNode;
 }) => {
   const [value, setValue] = React.useState(0);
-
   const handleChange = React.useCallback(
     (_: React.ChangeEvent<{}>, newValue: number) => setValue(newValue),
     [setValue]
   );
+  const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       <Tabs
         orientation="vertical"
         value={value}
         onChange={handleChange}
         aria-label="Vertical Navigation Tabs"
+        className={classes.tabs}
       >
         {items.map((item, i) => (
           <Tab
@@ -39,6 +54,7 @@ const VerticalNavigationTabs = ({
         role="tabpanel"
         id={`nav-tabpanel-${value}`}
         aria-labelledby={`nav-tab-${value}`}
+        className={classes.tabPanel}
       >
         {children}
       </div>
