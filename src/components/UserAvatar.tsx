b2e4f470a093ccddd06d8c1174b012cab0c6bd79
David Vargas <dvargas92495@gmail.com>
2020-11-29 12:33:02 -0500

    Avatar component is a

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,8 +1,15 @@
 import Avatar from "@material-ui/core/Avatar";
 import React from "react";
+import { BASE_PATH } from "./util";
 
-const UserAvatar = (user: { name: string; avatarUrl: string }) => (
-  <Avatar alt={user.name} src={user.avatarUrl} />
+const UserAvatar = ({
+  name,
+  avatarUrl,
+}: {
+  name: string;
+  avatarUrl: string;
+}) => (
+  <Avatar alt={name} src={avatarUrl} component="a" href={`${BASE_PATH}user`} />
 );
 
 export default UserAvatar;
