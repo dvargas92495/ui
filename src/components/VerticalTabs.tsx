545482098cd9951a86bc32af8a5863295fc36c09
David Vargas <dvargas92495@gmail.com>
2021-01-09 15:11:30 -0500

    Vert tab buffs

--- src/components/VerticalTabs.tsx
+++ src/components/VerticalTabs.tsx
@@ -1,16 +1,45 @@
 import Paper from "@material-ui/core/Paper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import React from "react";
 import H4 from "./H4";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    display: "flex",
+  },
+  tabPanel: {
+    paddingLeft: theme.spacing(2),
+    width: "100%",
+  },
+  /*
+  tabsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    minWidth: theme.spacing(34),
+  },
+  tabs: {
+    color: theme.palette.getContrastText(theme.palette.text.primary),
+    backgroundColor: theme.palette.text.primary,
+    flexGrow: 1,
+  },
+  titleContainer: {
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(2),
+  },
+  title: {
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+    marginBottom: 0,
+  },*/
+}));
+
 const VerticalTabs = ({
-  items,
   children,
   title,
 }: {
-  items: string[];
-  children: React.ReactNodeArray;
+  children: React.ReactElement<{ label: string }>[];
   title: React.ReactNode;
 }) => {
   const [value, setValue] = React.useState(0);
@@ -18,13 +47,13 @@ const VerticalTabs = ({
     (_: React.ChangeEvent<{}>, newValue: number) => setValue(newValue),
     [setValue]
   );
+  const classes = useStyles();
+  const items = children.map((c) => c.props.label);
 
   return (
-    <div>
-      <div>
-        <Paper variant="outlined" square>
-          <H4>{title}</H4>
-        </Paper>
+    <div className={classes.root}>
+      <Paper variant="elevation" square>
+        <H4>{title}</H4>
         <Tabs
           orientation="vertical"
           value={value}
@@ -36,15 +65,16 @@ const VerticalTabs = ({
               key={i}
               label={item}
               id={`vert-tab-${i}`}
-              aria-controls={`nav-tabpanel-${i}`}
+              aria-controls={`vert-tabpanel-${i}`}
             />
           ))}
         </Tabs>
-      </div>
+      </Paper>
       <div
         role="tabpanel"
         id={`vert-tabpanel-${value}`}
         aria-labelledby={`vert-tab-${value}`}
+        className={classes.tabPanel}
       >
         {children[value]}
       </div>
