ff9d1fa75f4350cfb1872b0f6a618451020e91ce
David Vargas <dvargas92495@gmail.com>
2021-03-21 13:01:54 -0500

    Toggle visibility on String Field

--- src/components/StringField.tsx
+++ src/components/StringField.tsx
@@ -1,12 +1,38 @@
 import TextField from "@material-ui/core/TextField";
-import React, { ChangeEvent, useCallback } from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
 import { FieldComponent } from "./util";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { IconButton, InputAdornment } from "@material-ui/core";
 
-const StringField: FieldComponent<string> = ({ value, setValue, ...props }) => {
+type ExtraProps = { toggleable?: boolean };
+
+const useInputProps = ({toggleable}: ExtraProps) => {
+  const [show, setShow] = useState(false);
+  if (toggleable) {
+    return {
+      type: show ? "text" : "password",
+      InputProps: {
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={() => setShow(!show)}>
+              {show ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      },
+    };
+  } else {
+    return {};
+  }
+};
+
+const StringField: FieldComponent<string, { toggleable?: boolean }> = ({ value, setValue, ...props }) => {
   const onChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value || ""),
     [setValue]
   );
+  const inputProps = useInputProps(props);
   return (
     <TextField
       {...props}
@@ -14,6 +40,7 @@ const StringField: FieldComponent<string> = ({ value, setValue, ...props }) => {
       type={props.name?.toLowerCase() === "password" ? "password" : "text"}
       onChange={onChange}
       placeholder={`Enter ${props.label}...`}
+      {...inputProps}
     />
   );
 };
