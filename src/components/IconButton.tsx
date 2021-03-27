c159d1a8701e4da2c49e5e19079d0418701d1e3d
David Vargas <dvargas92495@gmail.com>
2021-03-27 16:36:12 -0500

    Delete Icon Button

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,11 +3,13 @@ import MuiIconButton, { IconButtonProps } from "@material-ui/core/IconButton";
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
 import ChevronRight from "@material-ui/icons/ChevronRight";
 import FileCopy from "@material-ui/icons/FileCopy";
+import Delete from "@material-ui/icons/Delete";
 
 const ICONS = {
   chevronLeft: ChevronLeft,
   chevronRight: ChevronRight,
   fileCopy: FileCopy,
+  delete: Delete,
 };
 
 const IconButton = ({
