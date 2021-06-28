7912cb376a13bba9e2be7eb7ae106546c6eb5961
David Vargas <dvargas92495@gmail.com>
2021-06-28 15:12:51 -0400

    add contributor emojis

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -4,34 +4,56 @@ import Link from "@material-ui/core/Link";
 import React from "react";
 import { isBrowser } from "react-device-detect";
 import Subtitle from "./Subtitle";
+import Tooltip from "./Tooltip";
 
-type Sponsor = { title: string; imgSrc: string; url: string };
+const emojisToTooltip = {
+  "💵": "Financial",
+  "📓": "Testing",
+  "🤔": "Idea",
+  "💻": "Code",
+  "📖": "Documentation",
+};
 
-const ThankYou: React.FunctionComponent<{ sponsors: Sponsor[] }> = ({
-  sponsors,
-}) => (
+type Sponsor = {
+  title: string;
+  imgSrc?: string;
+  url?: string;
+  emojis?: (keyof typeof emojisToTooltip)[];
+};
+
+const ThankYou: React.FunctionComponent<{
+  sponsors: Sponsor[];
+  defaultImgSrc: string;
+}> = ({ sponsors, defaultImgSrc }) => (
   <Grid container style={{ margin: "16px 0" }} spacing={1}>
-    {sponsors.map(({ title, imgSrc, url }) => (
-      <Grid item xs={isBrowser ? 1 : 4} key={title}>
-        <Card
-          style={{
-            textAlign: "center",
-            minHeight: 108,
-          }}
-        >
-          <img src={imgSrc} alt={title} style={{ width: "100%" }} />
-          <Subtitle style={{ fontSize: "0.5em" }}>
-            {url ? (
-              <Link href={url} target="_blank" rel="noopener">
-                {title}
-              </Link>
-            ) : (
-              title
-            )}
-          </Subtitle>
-        </Card>
-      </Grid>
-    ))}
+    {sponsors.map(
+      ({ title, imgSrc = defaultImgSrc, url = "", emojis = [] }) => (
+        <Grid item xs={isBrowser ? 1 : 4} key={title}>
+          <Card
+            style={{
+              textAlign: "center",
+              minHeight: 108,
+            }}
+          >
+            <img src={imgSrc} alt={title} style={{ width: "100%" }} />
+            <Subtitle style={{ fontSize: "0.5em" }}>
+              {url ? (
+                <Link href={url} target="_blank" rel="noopener">
+                  {title}
+                </Link>
+              ) : (
+                title
+              )}
+            </Subtitle>
+            {emojis.map((s) => (
+              <Tooltip title={emojisToTooltip[s]} key={s}>
+                <span style={{ cursor: "help" }}>{s}</span>
+              </Tooltip>
+            ))}
+          </Card>
+        </Grid>
+      )
+    )}
   </Grid>
 );
 
