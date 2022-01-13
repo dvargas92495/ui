72ddfcc3c031da88b36e95f81147d360bdc59d9f
David Vargas <dvargas92495@gmail.com>
2022-01-13 15:11:15 -0600

    fix button import

--- src/components/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog.tsx
@@ -1,5 +1,5 @@
 import { CircularProgress, makeStyles } from "@material-ui/core";
-import MuiButton, { ButtonProps } from "@material-mui/core/Button";
+import MuiButton, { ButtonProps } from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
