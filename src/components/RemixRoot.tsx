2322f51805aaf9ec9261d6b9e26c6154b99ff618
David Vargas <dvargas92495@gmail.com>
2022-04-06 14:42:22 -0400

    h full classes to html and body

--- src/components/RemixRoot.tsx
+++ src/components/RemixRoot.tsx
@@ -66,14 +66,14 @@ type Props = {};
 const App = () => {
   const data = useLoaderData<{ ENV: Record<string, string> }>();
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         <Outlet />
         <ScrollRestoration />
         <script
