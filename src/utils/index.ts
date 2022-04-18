4f46c64041aad3a7769f28866ab5407099b4daad
David Vargas <dvargas92495@gmail.com>
2022-04-18 00:52:44 -0400

    what is going on with typescript

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as clerkUserProfileCss } from "./clerkUserProfileCss";
-export { default as getHandler } from "./getHandler";
-export { default as getMeta } from "./getMeta";
-export { default as getRemixConfig } from "./getRemixConfig";
-export { default as getRootAppLoader } from "./remixAppLoader.server";
-export { default as getRootAuthedLoader } from "./remixAuthedLoader.server";
-export { default as useHandler } from "./useHandler";
-export { default as useAuthenticatedHandler } from "./useAuthenticatedHandler";
