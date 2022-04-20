25d717e290f243c361d0fb01842c8e9d4858acdc
David Vargas <dvargas92495@gmail.com>
2022-04-20 17:12:20 -0400

    exists sync on patch

--- patch.js
+++ patch.js
@@ -2,5 +2,7 @@ const fs = require("fs");
 
 const brokenFile =
   "node_modules/@clerk/backend-core/src/api/utils/RestClient.ts";
-const content = fs.readFileSync(brokenFile).toString();
-fs.writeFileSync(brokenFile, content.replace("let body;", "let body: any;"));
+if (fs.existsSync(brokenFile)) {
+  const content = fs.readFileSync(brokenFile).toString();
+  fs.writeFileSync(brokenFile, content.replace("let body;", "let body: any;"));
+}
