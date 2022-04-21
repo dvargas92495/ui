4f1a265aa07d59f948d40b5a233241f0bb56208f
David Vargas <dvargas92495@gmail.com>
2022-04-21 14:54:38 -0400

    hydrate root and <= peer deps

--- src/components/EntryClient.tsx
+++ src/components/EntryClient.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { hydrate } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import { RemixBrowser } from "@remix-run/react";
 
-export default () =>
-  hydrate(
-      // TODO fix types here, what's going on...
-    ((<RemixBrowser />) as unknown) as React.ReactElement[],
-    (document as unknown) as Element
-  );
+export default () => hydrateRoot(document, <RemixBrowser />);
