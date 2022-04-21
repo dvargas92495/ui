850c54458777f398a528c57ec5b04e307da8ac24
David Vargas <dvargas92495@gmail.com>
2022-04-20 23:26:06 -0400

    Table changes in v8

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,31 +1,49 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
-  useTable,
-  usePagination,
-  UsePaginationInstanceProps,
-  TableInstance,
-  UsePaginationState,
-  UsePaginationOptions,
-  TableOptions,
-} from "react-table";
+  createTable,
+  useTableInstance,
+  getCoreRowModelSync,
+  getColumnFilteredRowModelSync,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
 import { useLoaderData, useSearchParams } from "@remix-run/react";
 
-type PaginationTableInstance<T extends object> = TableInstance<T> &
-  UsePaginationInstanceProps<T> & {
-    state: UsePaginationState<T>;
-  };
-
-type Options<T extends object> = TableOptions<T> &
-  UsePaginationOptions<T> & {
-    initialState: UsePaginationState<T>;
-  };
+const table = createTable();
 
 const Table = ({
   onRowClick,
+  className = "border-2 border-blue-600 w-full",
+  thClassName = "border-b-2 border-b-red-500 bg-sky-400 text-black font-bold cursor-pointer",
+  theadClassName = "",
+  getTrClassName = (index: number) =>
+    `cursor-pointer ${
+      index % 2 === 0 ? "bg-gray-200" : "bg-gray-300"
+    } hover:bg-gray-400`,
+  getTdClassName = () => `p-3 border-2 border-gray-500`,
 }: {
   onRowClick?: (row: any, index: number) => void;
+  className?: string;
+  thClassName?: string;
+  theadClassName?: string;
+  getTrClassName?: (index: number) => string;
+  getTdClassName?: (index: number) => string;
 }) => {
-  const { data, columns, count } = useLoaderData();
+  const { data, columns: loaderColumns, count } = useLoaderData<{
+    count: number;
+    columns: { Header: string; accessor: string }[];
+    data: {}[];
+  }>();
+  const columns = React.useMemo(
+    () =>
+      table.createColumns(
+        loaderColumns.map((col) =>
+          table.createDataColumn(col.accessor, {
+            header: col.Header,
+          })
+        )
+      ),
+    []
+  );
 
   const [searchParams, setSearchParams] = useSearchParams();
   const index = Number(searchParams.get("index")) || 0;
@@ -33,75 +51,73 @@ const Table = ({
   const {
     getTableProps,
     getTableBodyProps,
-    headerGroups,
-    page,
-    prepareRow,
-    canNextPage,
-    canPreviousPage,
-    gotoPage,
+    getHeaderGroups,
+    getRowModel,
+    getCanNextPage,
+    getCanPreviousPage,
+    setPageIndex,
     previousPage,
     nextPage,
-    pageCount,
-    pageOptions,
+    getPageCount,
+    getPageOptions,
     setPageSize,
-    state: { pageIndex, pageSize },
-  } = useTable(
-    {
-      columns,
-      data,
-      initialState: {
+    getState,
+  } = useTableInstance(table, {
+    columns,
+    data,
+    state: {
+      pagination: {
         pageIndex: index,
         pageSize: size,
+        pageCount: count,
       },
-      manualPagination: true,
-      pageCount: Math.ceil((count || data.length) / size),
-    } as Options<typeof data>,
-    usePagination
-  ) as PaginationTableInstance<typeof data>;
-  useEffect(() => {
-    if (index !== pageIndex || size !== pageSize) {
-      setSearchParams({
-        index: pageIndex.toString(),
-        size: pageSize.toString(),
-      });
-    }
-  }, [index, size, setSearchParams, pageIndex, pageSize]);
+    },
+
+    getCoreRowModel: getCoreRowModelSync(),
+    getColumnFilteredRowModel: getColumnFilteredRowModelSync(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: (val) => {
+      if (typeof val !== "function") {
+        setSearchParams({
+          index: val.pageIndex.toString(),
+          size: val.pageSize.toString(),
+        });
+      }
+    },
+  });
+  const {
+    pagination: { pageIndex, pageSize },
+  } = getState();
 
   return (
-    <>
-      <table {...getTableProps()} className="border-2 border-blue-600">
-        <thead>
-          {headerGroups.map((headerGroup) => (
+    <div className="w-full">
+      <table {...getTableProps()} className={className}>
+        <thead className={theadClassName}>
+          {getHeaderGroups().map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps()}
-                  className="border-b-2 border-b-red-500 bg-sky-400 text-black font-bold"
-                >
-                  {column.render("Header")}
+                <th {...column.getHeaderProps()} className={thClassName}>
+                  {column.renderHeader()}
                 </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {page.map((row, index) => {
-            prepareRow(row);
+          {getRowModel().rows.map((row, index) => {
             return (
               <tr
                 {...row.getRowProps()}
-                className={`cursor-pointer ${
-                  index % 2 === 0 ? "bg-gray-200" : "bg-gray-300"
-                } hover:bg-gray-400`}
+                className={getTrClassName(index)}
                 onClick={() => onRowClick?.(data[index], index)}
               >
-                {row.cells.map((cell) => {
+                {row.getAllCells().map((cell, jndex) => {
                   return (
                     <td
                       {...cell.getCellProps()}
-                      className={`p-3 border-2 border-gray-500`}
+                      className={getTdClassName(jndex)}
                     >
-                      {cell.render("Cell")}
+                      {cell.renderCell()}
                     </td>
                   );
                 })}
@@ -110,51 +126,66 @@ const Table = ({
           })}
         </tbody>
       </table>
-      <div>
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {"<<"}
-        </button>{" "}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {"<"}
-        </button>{" "}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {">"}
-        </button>{" "}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {">>"}
-        </button>{" "}
-        <span>
-          Page{" "}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>{" "}
-        </span>
-        <span>
-          | Go to page:{" "}
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              gotoPage(page);
-            }}
-            style={{ width: "100px" }}
-          />
-        </span>{" "}
-        <select
-          value={pageSize}
-          onChange={(e) => {
-            setPageSize(Number(e.target.value));
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-    </>
+      {getPageOptions().length > 1 && (
+        <div className="w-full flex justify-space-between items-center">
+          <div>
+            <button
+              onClick={() => setPageIndex(0)}
+              disabled={!getCanPreviousPage()}
+            >
+              {"<<"}
+            </button>
+            <button
+              onClick={() => previousPage()}
+              disabled={!getCanPreviousPage()}
+            >
+              {"<"}
+            </button>
+            <button onClick={() => nextPage()} disabled={!getCanNextPage()}>
+              {">"}
+            </button>
+            <button
+              onClick={() => setPageIndex(getPageCount() - 1)}
+              disabled={!getCanNextPage()}
+            >
+              {">>"}
+            </button>
+          </div>
+          <div>
+            <span>
+              Page
+              <strong>
+                {pageIndex + 1} of {getPageOptions().length}
+              </strong>
+            </span>
+            <span>
+              | Go to page:
+              <input
+                type="number"
+                defaultValue={pageIndex + 1}
+                onChange={(e) => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                  setPageIndex(page);
+                }}
+                className={" w-24"}
+              />
+            </span>
+            <select
+              value={pageSize}
+              onChange={(e) => {
+                setPageSize(Number(e.target.value));
+              }}
+            >
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
