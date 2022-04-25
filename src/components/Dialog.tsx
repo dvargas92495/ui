0664f15e1c9808ce963d2c2425ea96d5852fc5b6
David Vargas <dvargas92495@gmail.com>
2022-04-25 11:41:25 -0400

    include content classname too

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -8,11 +8,13 @@ const Dialog = ({
   title,
   children,
   className = "fixed inset-0 z-30 overflow-y-auto",
+  contentClassName = "inline-block w-96 max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl",
 }: React.PropsWithChildren<{
   isOpen: boolean;
   onClose: () => void;
   title: React.ReactNode;
   className?: string;
+  contentClassName?: string;
 }>) => {
   const actionData = useActionData();
   useEffect(() => {
@@ -47,7 +49,7 @@ const Dialog = ({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="inline-block w-96 max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+            <div className={contentClassName}>
               <HeadlessDialog.Title
                 as="h3"
                 className="text-lg font-medium leading-6 text-gray-900"
